bcacf398358b3e324b227c0237968379f9b61fcc
Glenn Cottam <glenncottam@mackwnox.ca>
2019-10-03 18:33:19 -0300

    Updated Functions
    The function for extracting the command has been simplified, which also makes the if statements for "server reactions" to be more efficiant.

--- functions.js
+++ functions.js
@@ -1,11 +1,22 @@
-function remove_command(text)
+var methods = {};
+
+//      Function to search a message for a specific term
+// at the beginning of the string.
+methods.split_message = function(msg)
 {
-    var arguments;
-    arguments = text.split(" ");
-    return arguments;
+    var array = [1];
+    var full_message;
+
+    // Splits entire message into an array (divided by spaces)
+    full_message = msg.content.split(' ');
+    array[0] = full_message[0];
+    full_message.shift();
+    full_message = full_message.join(' ');
+    array[1] = full_message;
+    
+    return array;
 }
 
-module.exports =
-{
-    remove_command()
-}
+// Exports functions to be used by other programs
+// Check out require('./functions.js') under index.js
+module.exports = methods;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 // For token, read README.md
 // Grabs Token from config.js.
 const config = require('./config');
-// const functions = require('./functions');
+const functions = require('./functions');
 
 // This token is used to log in RacTrack, do not change it unless you know what your doing.
 const token = config.token;
@@ -14,85 +14,85 @@ const token = config.token;
 //      @user pong
 const ident = config.ident;
 
+// If token is missing, close the program
 if(config.token === "")
 {
     console.log("No token, please get token to run.");
+    process.exit();
 }
+
+// Logs in RacTrack bot
+client.login(token);
 
 // When Bot is ready
 client.on('ready', () => 
 {
     console.log('Logged in as: ' + client.user.tag + "\nBot Ready!");
-    
 });
 
 // Each time someone sends a message, the bot will scan it
 client.on('message', msg =>
 {
-    var message = search_message(msg, ";search");
-    if(message != 0)
-    {
-        console.log("Message Value: " + message);
-        msg.reply("Searching for: " + message);
-        // ACTUALLY SEARCH SOMETHING
 
+    // Function that returns an array in order to declare functions
+    // depending on what the discord bot needs to do.
+    // Returning array: [0] = command, [1] = argument.
+    var message = functions.split_message(msg);
+    var username = msg.author.username;
+
+    // If it finds the term we wanted to search for:
+    if(message[0] === ";search")
+    {
+        console.log("\nSearch Command Found: " + message[1] + "\nSent by: " + username);
+        msg.reply("Searching for: " + message[1]);
+        // ACTUALLY SEARCH SOMETHING
+    }
+    // Testing
+    else if(message[0] === ";thanks")
+    {
+        msg.reply("No probs fam");
+    }
+
+    else if(message[0] === ";help")
+    {
+        console.log("Helping: ", + username);
+        msg.reply("No.");
     }
 
     // Current test statement
-    if(msg.content === ident + 'ping')
+    else if(msg.content === ident + 'ping')
     {
-        console.log("Pinged by: " + msg.author);
+        console.log("\nPinged by: " + username);
         msg.reply('pong');
     }
-    if(msg.content === ident + 'penis')
+
+    // Prints out something just horrible. 
+    // Don't include this in the final code...
+    else if(msg.content === ident + 'penis')
     {
         var length = (Math.random() * 10) + 1;
         // console.log("Someone got a small thingy: " + length);
         var message = "8";
+        console.log(username + " has a " + length + " penis");
         while(length > 0)
         {
             message += "="
             length --;
         }
         message += "D"
+
         // console.log(message);
         msg.reply(message); 
     }
-
 });
 
-// Logs in RacTrack bot
-client.login(token);
-
-function search_message(msg, term)
-{
-    var full_message = msg.content.split(' ');
-    if(full_message[0] === term)
-    {
-        full_message[0] = null;         // Removes command
-        full_message = full_message.join(' ');
-        full_message = full_message.substr(1);
-        return full_message;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-// Functions
-function remove_command(text)
-{
-    var arguments;
-    arguments = text.split(" ");
-    return arguments;
-}
-
+// Destroys client for Bot Logout
 process.on('exit', () =>
 {
     client.destroy();
 });
 
+// Destroys client on [CTRL-C] / kill commands
 process.on('SIGINT', () =>
 {
     client.destroy();
